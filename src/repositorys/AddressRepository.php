56b9b4d41de809d37d3edcc44c4e72eb2bf71269
Braio <mernickbryan@gmail.com>
2024-02-26 22:28:35 -0300

    fix: inicio da implementação da api openstreetmap

--- src/repositorys/AddressRepository.php
+++ src/repositorys/AddressRepository.php
@@ -21,8 +21,28 @@ class AddressRepository
 
     public function getAddress($address)
     {
-        //TODO - requisição api para pegar a coordenada por latitude e longitude
-        return null;
+        //TODO - realizar a conexão com a api correta
+        $url = 'https://nominatim.openstreetmap.org/search?format=json&q=' . urlencode($address);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+
+        if ($response === false) {
+            die(curl_error($curl));
+        }
+
+        $data = json_decode($response, true);
+
+        if (empty($data)) {
+            return null;
+        }
+
+        $lat = $data[0]['lat'];
+        $lon = $data[0]['lon'];
+
+        return array('latitude' => $lat, 'longitude' => $lon);
     }
 
     public function getAddressByZipCode($zipCode)
